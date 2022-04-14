b68f7bde9da29c89bdd58492d39324c7717b7586
Ferdimuhammad <101495626+Ferdimuhammad@users.noreply.github.com>
2022-04-15 00:38:11 +0700

    Update group-welcome.js

--- plugins/group-welcome.js
+++ plugins/group-welcome.js
@@ -1,16 +1,28 @@
-/*
-   Silahkan Di Pakek
-   Tapi Bantu Rapihin :v
-   Buatan: Miaweers
-*/
-
-let handler = async (m, { conn }) => {
-  let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-  conn.reply(m.chat, `「 List Premium 」` + `\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, m, { contextInfo: { mentionedJid: prem } })
+let moment = require('moment-timezone')
+let fs = require('fs')
+let handler = async (m, { conn, command, text }) => {
+let img = './src/WARD.png'
+let user = global.db.data.users[m.sender]
+conn.send2Button(m.chat, `Silahkan pilih button dibawah untuk mengaktifkan fitur welcome`, 'WarDBOTZ', 'On', '#on welcome', 'Off', '#off welcome', { key: { fromMe: false, remoteJid: '6283136505591-1614953337@g.us', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `${ucapan()} ${conn.getName(m.sender)}`, itemCount: 2022, thumbnail: fs.readFileSync('./src/WARD.jpg')
+}}})
 }
-handler.help = ['premlist']
-handler.tags = ['owner']
-handler.command = /^(listprem|premlist)$/i
-handler.owner = true
+handler.command = /^welcome$/i
 
 module.exports = handler
+function ucapan() {
+    const time = moment.tz('Asia/Jakarta').format('HH')
+    res = "Selamat dinihari🌖"
+    if (time >= 4) {
+        res = "Selamat pagi🌝"
+    }
+    if (time > 10) {
+        res = "Selamat siang🌞"
+    }
+    if (time >= 15) {
+        res = "Selamat sore🌕"
+    }
+    if (time >= 18) {
+        res = "Selamat malam🌚"
+    }
+    return res
+}
